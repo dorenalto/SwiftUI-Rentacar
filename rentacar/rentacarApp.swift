c9ea56034c459dec5dd83ca27e4d313d5aecc8b6
Dorenalto M Couto <dorenalto@gmail.com>
2024-11-24 20:15:14 -0300

    Initial Commit

---
+++ rentacar/rentacarApp.swift
@@ -0,0 +1,17 @@
+//
+//  rentacarApp.swift
+//  rentacar
+//
+//  Created by dorenalto mangueira couto on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct rentacarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
